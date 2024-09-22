0a8846939e1af4e9260ab8da85493abd2093a37c
RaulAM <raulartilesm@gmail.com>
2024-09-22 22:21:02 +0100

    Update hospital rapida

--- ejerciciosClase-1809.js
+++ ejerciciosClase-1809.js
@@ -328,37 +328,5 @@ function masCuatroLetras (element, index) {
 const solucionOldARR2 = oldARR2.filter(masCuatroLetras)
 console.log(solucionOldARR2)
 
-// 4. Filtrar productos en stock y con precio menor a 50
 
-const productos = [
-    { nombre: "Camisa", precio: 30, enStock: true },
-    { nombre: "Pantalón", precio: 60, enStock: true },
-    { nombre: "Zapatos", precio: 45, enStock: false },
-    { nombre: "Sombrero", precio: 70, enStock: true }
-];
-
-const enStockYCincuenta = productos.filter(function (element, index) {
-
-    let newObject = {}
-    
-    let arrKeys = Object.keys(productos)
-
-    for (let i = 0 ; i < arrKeys.length ; i++)
-
-        if (element.enStock === true){
-
-            if (element.precio > 50 ) {
-
-                newObject[arrKeys] = element[arrKeys]
-                
-                return newObject
-            }
-        }
-} )
-
-console.log(enStockYCincuenta)
-
-
-
-// 5. Filtrar elementos únicos de un array
 
